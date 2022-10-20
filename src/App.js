e211e1dd0a3a4e27255e0d68e32be01be5269e0c
Stella Raab <hallo@stellaraab.de>
2022-10-20 12:23:04 +0200

    Added metadata, favicon and set up hero component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Hero from "./components/Hero";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-5xl">Hello! This is Anita Pizza!</h1>
-      <p>We make the best pizza for you, and this is true.</p>
-    </div>
+    <>
+    <Hero />
+    </>
   );
 }
 
