ac8e669c78f0d225675bf242a81b3e04a3a769a6
Stella Raab <hallo@stellaraab.de>
2022-10-22 23:39:31 +0200

    Added spinning pizza and price tag

--- src/components/Pizzacard.jsx
+++ src/components/Pizzacard.jsx
@@ -4,6 +4,7 @@ import {data} from '../data/data.js'
 const Pizzacard = ({item}) => {
   return (
     <div className='pizzacard relative flex flex-col justify-between rounded-[20px] hover:shadow-xl hover:scale-105 duration-300 group'>
+    <p className="absolute bottom-0 right-0 px-3 py-2 rounded-tl-[20px] rounded-br-[20px] text-[#FFE8EC] font-bold bg-[#FF1842] lining-nums">{item.exactprice} €</p>
     <div className="absolute z-[15] top-[-144px] sm:top-[-120px] md:top-[-100px] lg:top-[-103px] xl:top-[-130px] w-full">
       <img
         src={item.image}
@@ -11,12 +12,12 @@ const Pizzacard = ({item}) => {
         className={`${item.class} h-[280px] sm:h-[240px] md:h-[200px] lg:h-[206px] xl:h-[260px] w-[280px] sm:w-[240px] md:w-[200px] lg:w-[206px] xl:w-[260px] block mx-auto`}
       />
     </div>
-    <div className="absolute top-[-134px] sm:top-[-110px] md:top-[-90px] lg:top-[-93px] xl:top-[-120px] w-full">
-      <div className="pizzashadow mx-auto h-[260px] sm:h-[220px] md:h-[180px] lg:h-[186px] xl:h-[240px] w-[260px] sm:w-[220px] md:w-[180px] lg:w-[186px] xl:w-[240px] bg-red-500 rounded-full"></div>
+    <div className="absolute top-[-124px] sm:top-[-100px] md:top-[-80px] lg:top-[-83px] xl:top-[-110px] w-full">
+      <div className="pizzashadow mx-auto h-[240px] sm:h-[200px] md:h-[160px] lg:h-[166px] xl:h-[220px] w-[240px] sm:w-[200px] md:w-[160px] lg:w-[166px] xl:w-[220px] bg-[#E5D1D5] rounded-full"></div>
     </div>
-      <div className="p-4 lg:p-6 mt-[130px] sm:mt-[120px] md:mt-[98px] lg:mt-[102px] xl:mt-[120px] flex flex-col gap-3">
+      <div className="px-4 pt-4 pb-12 lg:px-6 lg:pt-6 mt-[130px] sm:mt-[120px] md:mt-[98px] lg:mt-[102px] xl:mt-[120px] flex flex-col gap-3">
         <div className="flex text-[#FF4668] gap-2 w-full justify-center align-middle text-[15px]">
-          <p>{item.calories}kcal</p>•
+          <p className="lining-nums">{item.calories} kcal</p>•
           <div className="mt-1">{item.icon}</div>•
           <p>{item.tag}</p>
 
